7edab22d5a4adb9138e69c98ffe5f1e096d35872
Hemanshijs01 <164216766+Hemanshijs01@users.noreply.github.com>
2024-03-29 16:56:00 +0000

    convertion of datatype

---
+++ 01_basics/newfile.js
@@ -0,0 +1,19 @@
+//chaiAurCode Video 06 convertion of datatype
+
+let score =true;
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//"33"=> 33
+//"name"=> NaN
+//TRUE= 1, FALSE =0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
