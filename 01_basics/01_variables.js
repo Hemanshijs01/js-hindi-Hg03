5c666336ac1325ef4bcbbe1512978771d54d14e3
Hemanshijs01 <164216766+Hemanshijs01@users.noreply.github.com>
2024-03-29 15:42:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1445656
+let accountEmail = "hemanshuguaski@gmail.com"
+var accountPassword = "456446"
+accountCity ="jaipur"
+
+accountEmail = "jwejjewk@djd.com"
+accountPassword = "5467512"
+accountCity = "jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log(3+3) // code readiebility should be high 
+
+console.log("Hemanshi")
+ 
+let name ="hemanshi"
+let age = 23
+let loggedIn = false
+
+// number => 2 to power 53
+//bigint 
+//String =>""
+//bollean => true or false
+//null => standalone value
+//undefined=> 
+//symbol=> used for uniqueness
+
+//object
+    
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
