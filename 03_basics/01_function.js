6bef7f97f1b43b747e14570c42703e2f60d6f299
Hemanshijs01 <164216766+Hemanshijs01@users.noreply.github.com>
2024-04-17 15:51:40 +0000

    scopes

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -29,14 +29,44 @@
 
 // console.log("result:", result);
 
-function loginUserMessage(username = "sam"){
-    if(username===undefined){
-        console.log("please entre a username");
-        return
-    }
+// function loginUserMessage(username = "sam"){
+//     if(username===undefined){
+//         console.log("please entre a username");
+//         return
+//     }
 
-    return `${username} just loggerd in`
-}
+//     return `${username} just loggerd in`
+// }
 
 //console.log(loginUserMessage("hemanshi"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculatecartprice(val1,val2, ...num1){
+    return num1
+}
+
+//console.log(calculatecartprice(200,100,300,400));
+
+const user = {
+    username: "HEMANSHI",
+    fees: 130000
+}
+
+function handleObject(anyObject){
+    console.log(`username is${anyObject.username} and fees is${anyObject.fees}`);
+}
+
+handleObject(user)
+handleObject({
+    username : "vasu",
+    fees: 500000
+})
+
+const myNewArray = [100, 1000,500,6000,86]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 1000,500,6000,86]));
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,51 @@
+
+// if(true){
+//     let a =10;
+//     const b = 30;
+//     var c =50
+//     console.log(a);
+// }
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+// function one(){
+//     const userName = "Hemanshi"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(userName);
+//     }
+
+//     //console.log(website);
+
+//     two()
+// }
+
+// one()
+
+// if (true){
+//     const userName = "Hemasnhi"
+//     if (userName=== "Hemasnhi"){
+//         const website = "youtube"
+//         console.log(userName +" "+ website);
+//     }
+//     console.log(website);
+// }
+
+// console.log(userName);
+console.log(addone(5)
+);
+function addone(num){
+    return num +1
+}
+
+
+const addtwo = function(num){
+    return num +2
+}
+console.log(addtwo(5)
+);
+
+
