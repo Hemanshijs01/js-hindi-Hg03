86f212677b67729b7d1bdf5e5fb32df82c80fd65
Hemanshijs01 <164216766+Hemanshijs01@users.noreply.github.com>
2024-04-17 16:38:18 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,42 @@
+// const user = {
+//     username: "Hemanshi",
+//     fees: 999,
+
+//     welcomeMessage: function (){
+//         console.log(`${this.username} welcome to website !!`);
+//         console.log(this);
+//     }
+// }
+
+// user.welcomeMessage()
+
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "Hemanshi"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = ()=>{
+//     let username = "Hemanshi"
+//         console.log(this);   
+// }
+
+chai()
+
+// const addTwo = (num1, num2)=>{
+//     return num1 + num2
+// } This is known as Explicit return
+
+//const addTwo = (num1, num2)=>  num1 + num2
+
+//const addTwo = (num1, num2)=>  (num1 + num2) // This is known as Implicit return : we can directly use () brackets in place of {} with return word, both gives same results.
+// const addTwo = (num1,num2) => ({username: "Hemanshi"})
+
+// console.log(addTwo(3,4));
+
+// MimeTypeArray.forEach()
