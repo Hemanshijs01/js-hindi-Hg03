b52639775eea83c3a94d4bb61e3cb97d2dafa560
Hemanshijs01 <164216766+Hemanshijs01@users.noreply.github.com>
2024-05-13 16:35:41 +0000

    executioncontent

---
+++ 05_iterations/one.js
@@ -0,0 +1,49 @@
+// iterations or Loops
+
+//for loop
+
+// for (let i = 5; i < 10; i++) {
+//     const element = i;
+//     if(element==5){
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value:${i}`);
+//     for (let a = 1; a <= 10; a++) {
+//         //console.log(`Inner loop value:${a} and outer loop value:${i}`);
+//         // const element = array[a];
+//         console.log(i +'*' + a + '=' + i*a);
+//     }
+//     // const element = array[i];
+    
+// }
+
+// let myArray = ["a", "b", "c"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+//Key words : Break and continue
+
+// for (let index = 1; index <= 20; index++) {
+// if(index==5){
+//     console.log('detected 5');
+//     break
+// }
+
+// console.log(`value of i is ${index}`);    
+// }
+
+for (let index = 1; index <= 20; index++) {
+if(index==5){
+    console.log('detected 5');
+    continue
+}
+
+console.log(`value of i is ${index}`);    
+}
---
+++ 05_iterations/three.js
@@ -0,0 +1,20 @@
+//for of loop
+
+const arr=[1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings ="Hello world"
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`);
+}
+
+//Maps
+
+const map = new Map()
+map.set('Guj', 'India')
+map.set('United nation', 'UAE')
+
+console.log(map);
